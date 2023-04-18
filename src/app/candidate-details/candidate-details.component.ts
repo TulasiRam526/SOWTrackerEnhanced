a2542eee3e3349a558edfee0755f537fb268f049
TulasiRam526 <131161546+TulasiRam526@users.noreply.github.com>
2023-04-18 17:29:10 +0530

    Update candidate-details.component.ts

--- src/app/candidate-details/candidate-details.component.ts
+++ src/app/candidate-details/candidate-details.component.ts
@@ -14,6 +14,7 @@ import { LoginService } from '../services/login.service';
 })
 
 export class CandidateDetailsComponent implements OnInit {
+  
   CandidateList: any = [];
   model: any = {};
   submitted: boolean = false;
